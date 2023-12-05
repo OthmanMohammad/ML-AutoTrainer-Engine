8786e01480cd72a26f280dac1e2eba21087c5b70
Mohammad1 <emranm226@hotmail.com>
2023-12-05 09:56:49 +0000

    feat(streamlit): Implement iterative data processing - Introduced interim dataframe 'processed_data' for step-wise data transformations.
    - Enhanced user experience by allowing any-order operations without resetting previous changes.
    
    - Utilized Streamlit's session state to preserve data modifications between actions.

--- app.py
+++ app.py
@@ -9,45 +9,47 @@ def main():
 
     if uploaded_file:
         data = data_processing.read_csv(uploaded_file)
-        st.write(data.head())
+        if 'processed_data' not in st.session_state:
+            st.session_state.processed_data = data.copy()
 
-        target_column = streamlit_utils.select_target_column(data)
+        st.write(st.session_state.processed_data.head())
+
+        target_column = streamlit_utils.select_target_column(st.session_state.processed_data)
         st.write(f"Selected Target Column: {target_column}")
 
         # Drop columns section
         with st.expander("Drop Columns"):
-            columns_to_drop = st.multiselect("Select columns to drop", data.columns)
+            columns_to_drop = st.multiselect("Select columns to drop", st.session_state.processed_data.columns)
             if st.button("Drop Selected Columns"):
-                data = data_processing.drop_columns(data, columns_to_drop)
-                st.write(data.head())
+                st.session_state.processed_data = data_processing.drop_columns(st.session_state.processed_data, columns_to_drop)
+                st.write(st.session_state.processed_data.head())
 
         # Handle missing values section
         with st.expander("Handle Missing Values"):
             strategy = st.radio("Choose a strategy", ["Drop Rows", "Fill with Mean"])
             if st.button("Apply Strategy"):
                 if strategy == "Drop Rows":
-                    data = data_processing.handle_missing_values(data, strategy="drop")
+                    st.session_state.processed_data = data_processing.handle_missing_values(st.session_state.processed_data, strategy="drop")
                 elif strategy == "Fill with Mean":
-                    data = data_processing.handle_missing_values(data, strategy="mean")
-                st.write(data.head())
+                    st.session_state.processed_data = data_processing.handle_missing_values(st.session_state.processed_data, strategy="mean")
+                st.write(st.session_state.processed_data.head())
 
         # Convert categorical columns to numerical
         with st.expander("Convert Categorical Columns to Numerical"):
-            columns_to_convert = st.multiselect("Select columns to convert", data.columns)
+            columns_to_convert = st.multiselect("Select columns to convert", st.session_state.processed_data.columns)
             if columns_to_convert and st.button("Convert"):
-                data = data_processing.convert_categorical_to_numerical(data, columns_to_convert)
-                st.write(data.head())
-
+                st.session_state.processed_data = data_processing.convert_categorical_to_numerical(st.session_state.processed_data, columns_to_convert)
+                st.write(st.session_state.processed_data.head())
 
         # Filtering section
         with st.expander("Filter Data"):
-            filtered_data = streamlit_utils.apply_filters(data)
+            filtered_data = streamlit_utils.apply_filters(st.session_state.processed_data)
             if filtered_data is not None:
                 st.dataframe(filtered_data)
 
         # Model training section
         with st.expander("Model Training"):
-            results = streamlit_utils.train_model_ui(filtered_data or data, target_column)
+            results = streamlit_utils.train_model_ui(st.session_state.processed_data, target_column)
             if results:
                 st.write(results)
 
