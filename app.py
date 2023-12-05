3e0d5633e10d7c6ab481b53de26e4ccde2de7120
Mohammad1 <emranm226@hotmail.com>
2023-12-05 09:25:19 +0000

    Add feature to drop selected columns from uploaded CSV
    - Added drop_columns function in data_processing.py to handle column removal.
    - Integrated column drop feature in app.py, allowing users to select and remove unwanted columns from the data.
    - Display the updated dataset after dropping selected columns.
    
    Co-authored-by: Mo-TheNLPGuy <mohammad2github@outlook.com>

--- app.py
+++ app.py
@@ -3,13 +3,21 @@ from src import data_processing
 from src import streamlit_utils
 
 def main():
-    st.title("DataQueue AutoML App")
+    st.title("ML AutoTrainer Enginer")
 
     uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
 
     if uploaded_file:
         data = data_processing.read_csv(uploaded_file)
         st.write(data.head())
+
+        # Feature to select columns to drop
+        columns_to_drop = st.multiselect(
+            'Select columns to drop', data.columns)
+        if columns_to_drop:
+            data = data_processing.drop_columns(data, columns_to_drop)
+            st.write('Updated Data after dropping columns')
+            st.write(data.head())
 
         target_column = streamlit_utils.select_target_column(data)
         st.write(f"Selected Target Column: {target_column}")
@@ -19,6 +27,12 @@ def main():
             filtered_data = streamlit_utils.apply_filters(data)
             if filtered_data is not None:
                 st.dataframe(filtered_data)
+
+        # Model training section
+        with st.expander("Model Training"):
+            results = streamlit_utils.train_model_ui(filtered_data or data, target_column)
+            if results:
+                st.write(results)
 
 if __name__ == "__main__":
     main()
